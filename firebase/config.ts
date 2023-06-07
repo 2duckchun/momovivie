6a6465d0c344f4b4c8f65ec1e9794b5589897003
2DC <kkts9308@gmail.com>
2023-06-07 22:53:38 +0900

    파이어베이스 설정

---
+++ firebase/config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.FIRE_API_KEY,
+  authDomain: process.env.FIRE_AUTH_DOMAIN,
+  projectId: process.env.FIRE_PROJECT_ID,
+  storageBucket: process.env.FIRE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIRE_MESSAGING_SENDER_ID,
+  appId: process.env.FIRE_APP_ID,
+  measurementId: process.env.FIRE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
