a4a0d7e064362bff288e4a118a23e2925b8e19be
2DC <kkts9308@gmail.com>
2023-06-08 15:21:51 +0900

    movies.ts 타입 수정

--- types/movies.ts
+++ types/movies.ts
@@ -1,12 +1,19 @@
 export enum MovieApiUrl {
-  IMG = "https://image.tmdb.org/t/p/w500/",
+  IMG = "https://image.tmdb.org/t/p/w500",
   MOVIE_LIST_KO = "https://api.themoviedb.org/3/movie/top_rated?language=ko-KO",
   MOVIE_LIST_EN = "https://api.themoviedb.org/3/movie/top_rated?language=en-US",
+  MOVIE_DETAIL = "https://api.themoviedb.org/3/movie/",
 }
 
 export type MovieIndex = {
   params: {
     page: number;
+  };
+};
+
+export type MovieDetailId = {
+  params: {
+    id: number;
   };
 };
 
@@ -27,6 +34,47 @@ export type PopularMovie = {
   vote_count: number;
 };
 
-export type PopularMovieList = {
-  results?: Array<PopularMovie>;
+export type movieList = Array<PopularMovie>;
+
+export type GetMovieListResult = {
+  movieList: movieList | null;
+  isSuccess: boolean;
+};
+
+export type genre = {
+  id: number;
+  name: string;
+};
+
+export type MovieDetail = {
+  id: number;
+  adult: boolean;
+  backdrop_path: string;
+  genres: genre[];
+  overview: string;
+  title: string;
+  original_title: string;
+  vote_average: number;
+  release_date: string;
+};
+
+export type parsedMovieDetail = {
+  id: number;
+  adult: boolean;
+  backdrop_path: string;
+  genres: string[];
+  overview: string;
+  title: string;
+  original_title: string;
+  vote_average: number;
+  release_date: string;
+};
+
+export type MovieDetailInComponent = {
+  movieDetail: parsedMovieDetail;
+};
+
+export type GetMovieDetailResult = {
+  movieDetail: parsedMovieDetail | null;
+  isSuccess: boolean;
 };
