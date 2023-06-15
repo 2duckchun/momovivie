318dedab3876eb39f0510dd1adff9a858ea0870c
2DC <kkts9308@gmail.com>
2023-06-15 17:28:25 +0900

    timestamp convert util 로직 수정

--- utils/convertTimestamp.ts
+++ utils/convertTimestamp.ts
@@ -1,10 +1,20 @@
 import { Timestamp } from "firebase/firestore";
 
 export default function convertTimestamp(timestamp: Timestamp) {
-  let date = timestamp.toDate();
-  let mm = date.getMonth() + 1;
-  let dd = date.getDate();
-  let yy = date.getFullYear();
+  let date = timestamp?.toDate();
+  let month = date?.getMonth() + 1;
+  let day = date?.getDate();
+  let year = date?.getFullYear();
+  return `${month}/${day}/${year}`;
+}
 
-  return mm + "/" + dd + "/" + yy;
+export function convertTimestampToHourMinute(timestamp: Timestamp) {
+  let date = timestamp?.toDate();
+  let minute = date?.getMinutes();
+  let hour = date?.getHours();
+  let month = date?.getMonth() + 1;
+  let day = date?.getDate();
+  let year = date?.getFullYear();
+
+  return `${year}년 ${month}월 ${day}일 ${hour}시 ${minute}분`;
 }
