d042cae78d1aae545da68b567f66fd07a51ff0b0
2DC <kkts9308@gmail.com>
2023-06-15 17:31:46 +0900

    MovieDetail 컴포넌트 화면 일부 수정(요약 버튼 수정, 평점 toFixed 수정)

--- components/detail/MovieDetail.tsx
+++ components/detail/MovieDetail.tsx
@@ -22,17 +22,20 @@ export default function MovieDetail({ movieDetail }: MovieDetailInComponent) {
           style={imageStyle}
         ></Image>
       </div>
-      <h3>{movieDetail.title}</h3>
-      <p>({movieDetail.original_title})</p>
-      <p>{movieDetail.genres.join(", ").trim()}</p>
-      <details>
-        <summary>요약</summary>
-        <p>{movieDetail.overview}</p>
-      </details>
-      <p>별점: {movieDetail.vote_average.toFixed(2)}</p>
+      <div className="info-container">
+        <h3>{movieDetail.title}</h3>
+        <p>({movieDetail.original_title})</p>
+        <p>{movieDetail.genres.join(", ").trim()}</p>
+        <p>별점: {movieDetail.vote_average.toFixed(2)}</p>
+        <details>
+          <summary>요약</summary>
+          <p>{movieDetail.overview}</p>
+        </details>
+      </div>
       <style jsx>{`
         .container {
           background-color: #fff;
+          font-family: "GmarketSansMedium";
         }
         .img-container {
           margin: 0 auto;
@@ -41,6 +44,38 @@ export default function MovieDetail({ movieDetail }: MovieDetailInComponent) {
           align-items: center;
           width: 100%;
           padding: 20px;
+        }
+        .info-container {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        details {
+          width: 95%;
+          border: 1px solid #ccc;
+          border-radius: 10px;
+          padding: 0.5em 0.5em 0;
+          margin: 1rem auto 0.5rem;
+        }
+        summary {
+          font-weight: bold;
+          margin: -0.5em -0.5em 0;
+          padding: 0.5em;
+          cursor: pointer;
+        }
+        details[open] {
+          padding: 0.5em;
+        }
+        details[open] summary {
+          border-bottom: 1px solid #aaa;
+          margin-bottom: 0.5em;
+          background-color: #fff0c0;
+          border-radius: 10px 10px 0 0;
+        }
+        details p {
+          padding: 10px;
+          line-height: 1.2rem;
         }
       `}</style>
     </div>
