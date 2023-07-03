04fbf9334b92c8b9d11d7587868f1c672d43194d
2DC <kkts9308@gmail.com>
2023-07-03 16:07:19 +0900

    refactor : useTypingAnimation 훅 사용법 변경에 따른 컴포넌트 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   const welcomeMessage =
     "보고싶은 영화를 찾아보시고\n재미있게 보신 영화가 있으시다면\n감상평을 공유해주세요!";
 
-  const typing = useTypingAnimation(welcomeMessage);
+  const typing = useTypingAnimation(welcomeMessage, 50);
 
   return (
     <div className="container">
