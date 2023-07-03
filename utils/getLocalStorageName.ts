942e74c6ffdecd3f9366cfd8dbe31b6f24eca2e7
2DC <kkts9308@gmail.com>
2023-07-03 14:40:35 +0900

    refactor : 함수명 변경(useLocalName -> getlocalStorageName)

--- utils/getLocalStorageName.ts
+++ utils/getLocalStorageName.ts
@@ -1,4 +1,4 @@
-export default function useLocalName() {
+export default function getlocalStorageName() {
   const nickname = localStorage.getItem("nickname");
   return nickname ? nickname : "";
 }
