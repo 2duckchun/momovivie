c5cbbb04b88c65954c9128662efc634a07f1250e
2DC <kkts9308@gmail.com>
2023-07-03 14:44:01 +0900

    Refactor : 댓글 삭제 모달 디자인 수정 및 에러처리 구현

--- components/modal/CommentDeleteModal.tsx
+++ components/modal/CommentDeleteModal.tsx
@@ -12,10 +12,11 @@ export default function CommentDeleteModal({
   deleteComment: Function;
 }) {
   const [inputPassword, setInputPassword] = useState("");
+  const [error, setError] = useState(false);
 
   const handleData = (event: any) => {
-    if (event.target.id === "modal-input-password")
-      setInputPassword(event.target.value);
+    if (event.target.id === "modal-input-password") setError(false);
+    setInputPassword(event.target.value);
   };
 
   const checkPassword = (
@@ -26,8 +27,9 @@ export default function CommentDeleteModal({
     if (inputPassword === commentPassword) {
       deleteComment(commentId);
       setModal(false);
+    } else {
+      setError(true);
     }
-    // else 에러메세지 뿜고 비밀번호가 다르다고 나온다.
   };
 
   return (
@@ -43,14 +45,20 @@ export default function CommentDeleteModal({
             onChange={handleData}
             value={inputPassword}
           />
-          <button
-            onClick={() =>
-              checkPassword(inputPassword, commentPassword, commentId)
-            }
-          >
-            확인
-          </button>
-          <button onClick={() => setModal(false)}>취소</button>
+          {error && <p className="error-message">비밀번호가 다릅니다!</p>}
+          <div className="modal-button-container">
+            <button
+              className="confirm-button"
+              onClick={() =>
+                checkPassword(inputPassword, commentPassword, commentId)
+              }
+            >
+              확인
+            </button>
+            <button className="cancel-button" onClick={() => setModal(false)}>
+              취소
+            </button>
+          </div>
           <div>id : {commentId}</div>
           <div>pass : {commentPassword}</div>
         </div>
@@ -68,9 +76,41 @@ export default function CommentDeleteModal({
           align-items: center;
         }
         .modal-content {
+          overflow: hidden;
           width: 300px;
           background-color: #fff;
-          min-height: 150px;
+          min-height: 100px;
+          border-radius: 5px;
+          border: 1px solid #000;
+          box-shadow: 6px 6px 9px -2px rgba(0, 0, 0, 0.75);
+        }
+        h4 {
+          background-color: #bdb9b98a;
+        }
+        h4,
+        p {
+          text-align: center;
+          padding: 5px;
+        }
+        input {
+          margin: 0 auto;
+          width: 90%;
+          display: block;
+          padding: 2px 5px;
+        }
+        .modal-button-container {
+          display: flex;
+          padding: 10px 0px 10px;
+          margin: auto;
+          width: 90%;
+          justify-content: flex-end;
+          gap: 10px;
+        }
+        button {
+          padding: 3px 10px 3px;
+        }
+        .error-message {
+          color: tomato;
         }
       `}</style>
     </>
