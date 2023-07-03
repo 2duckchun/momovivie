5bf79fce79c2b842fdf869f058eeee97c3d16b9c
2DC <kkts9308@gmail.com>
2023-07-03 13:55:59 +0900

    feat : 캐러셀 슬라이스 컴포넌트 분리 및 화면 전환시 영화 디테일 라우팅 기능 개발

--- components/home/Carousel.tsx
+++ components/home/Carousel.tsx
@@ -1,59 +1,64 @@
+import { useCallback, useState } from "react";
+import useMoveToDetail from "@/hooks/useMoveToDetail";
 import Slider from "react-slick";
-import Image from "next/image";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { MOVIE_API_URL, MoviePoster } from "@/types/movies";
-
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  arrows: false,
-  pauseOnFocus: true,
-  pauseOnHover: true,
-  autoplaySpeed: 4000,
-};
-
-function CustionSlide({ movie }: any) {
-  return (
-    <div className="slider-container">
-      <Image
-        src={`${MOVIE_API_URL.IMG}${movie.poster_path}`}
-        alt={`${movie.title} 포스터`}
-        width={250}
-        height={350}
-      ></Image>
-      <h3>{movie.title}</h3>
-      <p>{movie.genres.join(", ")}</p>
-      <p>별점 : {movie.vote_average.toFixed(2)}</p>
-      <style jsx>{`
-        .slider-container {
-          width: 325px;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
-    </div>
-  );
-}
+import { MoviePoster } from "@/types/movies";
+import CarouselSlice from "./CarouselSlice";
+import Loading from "../share/Loading";
 
 export default function Carousel({ movieList }: { movieList?: MoviePoster[] }) {
+  const [dragging, setDragging] = useState(false);
+  const { moveToDetail } = useMoveToDetail();
+
+  const handleBeforeChange = useCallback(() => {
+    setDragging(true);
+  }, [dragging]);
+
+  const handleAfterChange = useCallback(() => {
+    setDragging(false);
+  }, [dragging]);
+
+  const moveTo = useCallback(
+    (id: number) => {
+      dragging ? null : moveToDetail(id);
+    },
+    [dragging]
+  );
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    autoplaySpeed: 4000,
+    draggable: true,
+    beforeChange: handleBeforeChange,
+    afterChange: handleAfterChange,
+  };
+
   return (
     <>
       {movieList ? (
         <section className="section-random-slider">
           <Slider {...settings}>
             {movieList.map((movie) => (
-              <CustionSlide movie={movie} />
+              <CarouselSlice
+                movie={movie}
+                key={movie.id}
+                onClickCard={moveTo}
+              />
             ))}
           </Slider>
         </section>
-      ) : null}
+      ) : (
+        <Loading />
+      )}
       <style jsx>{`
         .section-random-slider {
           margin: 0 auto;
---
+++ components/home/CarouselSlice.tsx
@@ -0,0 +1,34 @@
+import { MOVIE_API_URL } from "@/types/movies";
+import Image from "next/image";
+import { useCallback, useState } from "react";
+
+export default function CarouselSlide({ movie, onClickCard }: any) {
+  return (
+    <div className="slider-container" onClick={(e) => onClickCard(movie.id)}>
+      <Image
+        src={`${MOVIE_API_URL.IMG}${movie.poster_path}`}
+        alt={`${movie.title} 포스터`}
+        width={250}
+        height={350}
+        priority={true}
+      ></Image>
+      <h3>{movie.title}</h3>
+      <p>{movie.genres.join(", ")}</p>
+      <p>별점 : {movie.vote_average.toFixed(2)}</p>
+      <style jsx>{`
+        .slider-container {
+          width: 325px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          cursor: pointer;
+        }
+        h3,
+        p {
+          margin: 3px;
+        }
+      `}</style>
+    </div>
+  );
+}
