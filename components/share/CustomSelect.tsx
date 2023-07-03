f268e8b5e8cb340d55819e787c363e40067b30f2
2DC <kkts9308@gmail.com>
2023-07-03 14:46:51 +0900

    refactor : 의미없는 프래그먼트 제거

--- components/share/CustomSelect.tsx
+++ components/share/CustomSelect.tsx
@@ -3,13 +3,11 @@ import { useId } from "react";
 
 export default function CustomSelect({ option, setOption, initOption }: any) {
   return (
-    <>
-      <Select
-        instanceId={useId()}
-        value={option}
-        onChange={setOption}
-        options={initOption}
-      />
-    </>
+    <Select
+      instanceId={useId()}
+      value={option}
+      onChange={setOption}
+      options={initOption}
+    />
   );
 }
