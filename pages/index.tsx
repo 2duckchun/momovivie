66f81164e0b86399e6bc0cd8374bcdb40399037c
2DC <kkts9308@gmail.com>
2023-07-04 12:03:52 +0900

    refactor : index 페이지 타이핑애니메이션 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Seo from "@/components/Seo";
 import { useMemo } from "react";
-import Carousel from "@/components/home/Carousel";
+import Carousel from "@/components/main/Carousel";
 import useGetMovieCollectionList from "@/hooks/useGetRandomMovieList";
 import getRandomMovieList from "@/utils/getRandomMovieList";
-import useTypingAnimation from "@/hooks/useTypingAnimation";
 
 export default function Home() {
   const movieList = useGetMovieCollectionList();
@@ -14,13 +13,11 @@ export default function Home() {
   const welcomeMessage =
     "보고싶은 영화를 찾아보시고\n재미있게 보신 영화가 있으시다면\n감상평을 공유해주세요!";
 
-  const typing = useTypingAnimation(welcomeMessage, 50);
-
   return (
     <div className="container">
       <Seo title="Home | Momovivie"></Seo>
       <p className="welcome-message">
-        {typing}
+        {welcomeMessage}
         <span className="blink">|</span>
       </p>
       <Carousel movieList={randomMovieList} />
